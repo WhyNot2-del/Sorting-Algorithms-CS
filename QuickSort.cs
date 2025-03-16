e99bfac91fe477374a81361d08746487a5d68d40
Christopher Waschke <chrisdwaschke@gmail.com>
2025-03-15 23:05:27 -0700

    Add namespace to Quick Sort.

---
+++ QuickSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace algorithms;
+
+public class QuickSort
+{
+    public static void quickSort<T>(T[] data, int begin, int end) where T : IComparable
+    {
+
+        if (begin < end)
+        {
+            int partitionIndex = Partition(data, begin, end);
+
+            quickSort(data, begin, partitionIndex - 1);
+            quickSort(data, partitionIndex + 1, end);
+        }
+    }
+
+    private static int Partition<T>(T[] data, int begin, int end) where T : IComparable
+    {
+        T pivot = data[end];
+        int partitionIndex = begin - 1;
+        T temp;
+
+        for (int i = begin; end > i; i++)
+        {
+            if (data[i].CompareTo(pivot) == -1 || data[i].CompareTo(pivot) == 0)
+            {
+                partitionIndex++;
+                temp = data[partitionIndex];
+                data[partitionIndex] = data[i];
+                data[i] = temp;
+            }
+        }
+
+        temp = data[partitionIndex + 1];
+        data[partitionIndex + 1] = data[end];
+        data[end] = temp;
+
+        return partitionIndex + 1;
+    }
+}
