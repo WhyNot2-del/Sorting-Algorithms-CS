6ae13ecd9ddf6a181661dd028803dd688ec78439
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-03-18 15:44:22 -0700

    Added comments

--- QuickSort.cs
+++ QuickSort.cs
@@ -3,25 +3,31 @@
 namespace algorithms;
 
 public class QuickSort
-{
+{    
+    //  quickSort algorithm for sorting items
     public static void quickSort<T>(T[] data, int begin, int end) where T : IComparable
     {
-
+        //  base case for recursion, if there is only one or two items being sorted
         if (begin < end)
         {
+            //  breaks down the data into two smaller arrays
             int partitionIndex = Partition(data, begin, end);
 
+            //  sorts the two broken down arrays
             quickSort(data, begin, partitionIndex - 1);
             quickSort(data, partitionIndex + 1, end);
         }
     }
 
+    //  breaks down the data into smaller two arrays
     private static int Partition<T>(T[] data, int begin, int end) where T : IComparable
     {
         T pivot = data[end];
         int partitionIndex = begin - 1;
         T temp;
 
+        //  checks every value in current array
+        //  if value is smaller or the same as pivot value, swap the two values and increase the partitionIndex
         for (int i = begin; end > i; i++)
         {
             if (data[i].CompareTo(pivot) == -1 || data[i].CompareTo(pivot) == 0)
@@ -33,10 +39,12 @@ public class QuickSort
             }
         }
 
+        //  after all values have been checked, swap pivot value to final position
         temp = data[partitionIndex + 1];
         data[partitionIndex + 1] = data[end];
         data[end] = temp;
 
+        //  return index of the final position of the pivot value
         return partitionIndex + 1;
     }
 }
